876eeec63e41c8442c97d4981669fbabccd3b885
Thusajiny Ahilakumaran <ahilakumarant.22@uom.lk>
2025-03-29 13:43:40 +0530

    Update main.cpp

--- codes/main.cpp
+++ codes/main.cpp
@@ -7,7 +7,7 @@
 #include <DHTesp.h>
 
 #define NTP_SERVER "pool.ntp.org"
-#define UTC_OFFSET 0
+
 #define UTC_OFFSET_DST 0
 
 #define SCREEN_WIDTH 128
@@ -27,6 +27,9 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH,SCREEN_HEIGHT, &Wire, OLED_RESET);
 DHTesp dhtSensor;
 
+
+int UTC_OFFSET[]={0,0};
+
 int n_notes=8;
 int c=262;
 int d=294;
@@ -42,19 +45,19 @@ int days=0;
 int hours =0;
 int minutes =0;
 int seconds =0;
-
-bool alarm_enabled= false;
+bool alarm_enabled = false;
 int n_alarms =2;
-int alarm_hours[] = {0,0};
-int alarm_minutes[] = {1,10};
-int alarm_triggered[] = {false,false};
+int alarm_hours[] = {9,0};
+int alarm_minutes[] = {39,10};
+bool alarm_triggered[] = {false,false};
+bool alarm_active[] = {false,false};
 
 unsigned long timeNow=0;
 unsigned long timeLast=0;
 
 int current_mode =0;
-int max_modes = 4;
-String options[]={"1 - Set Time", "2 - Set Alarm 1", "3 - Set Alarm 2", "4-Remove Alarm"};
+int max_modes = 4; 
+String options[]={"1 - Set Time", "2 - Set Alarm", "3 - View Active Alarm", "4-Remove Alarm"};
 
 void print_line(String text, int text_size, int row, int column){
   display.setTextSize(text_size);
@@ -102,23 +105,48 @@ void ring_alarm(){
   while (digitalRead(CANCEL)==HIGH){
     for (int i=0; i< n_notes; i++){
       if (digitalRead(CANCEL)==LOW){
+        print_line("Alarm stopped",16,0,0);
         delay(200);
+        digitalWrite(LED_1, LOW);
         break;
       }
+      else if (digitalRead(OK)==LOW){
+        print_line("Alarm snoozed for 5 mins",16,0,0);
+        delay(5*60*1000);
+      }
+      tone(BUZZER, notes[i]);
+      delay(500);
+      noTone(BUZZER);
+      delay(200);
     }
   }
 }
 
-void update_time_with_check_alarm(){
+void update_time_with_check_alarm () {
   display.clearDisplay();
   update_time();
   print_time_now();
-  
-  if (alarm_enabled){
+  bool alarm_enabled = false;
+
+  if (alarm_active[0]==true){
+    alarm_enabled = true;
+  }
+  else if (alarm_active[1]==true){
+    alarm_enabled = true;
+  }
+  if (alarm_enabled ==  true){
+    print_line("00000000000000000",1,0,0);
+    delay(500);
+  }
+  if (alarm_enabled ==  true){
+    print_line("11111111111111111111111111",1,0,0);
     for (int i=0; i<n_alarms; i++){
-      if (alarm_triggered[i] ==false && hours == alarm_hours[i] && minutes==alarm_minutes[i]){
+      if (alarm_triggered[i] ==false & alarm_active[i] ==true & hours == alarm_hours[i] & minutes==alarm_minutes[i]){
+        display.clearDisplay();
+        print_line("2222222222222222222222",1,0,0);
         ring_alarm();
         alarm_triggered[i] = true;
+        alarm_active[i] = false;
       }
     }
   }
@@ -155,27 +183,25 @@ const char* wait_for_button_press(){
 }
 
 void set_time(){
-  int temp_hour =hours;
   while (true){
     display.clearDisplay();
-    print_line("Enter hour:" + String(temp_hour),0,0,2);
+    print_line("Enter hour from UTC:" + String(UTC_OFFSET[0]),0,0,2);
 
     String pressed = wait_for_button_press();
     if (pressed == "UP"){
       delay(200);
-      temp_hour +=1;
-      temp_hour= temp_hour % 24;
+      UTC_OFFSET[0] +=1;
+      UTC_OFFSET[0]= UTC_OFFSET[0] % 24;
     }
     else if (pressed == "DOWN"){
         delay(200);
-        temp_hour -= 1;
-        if (temp_hour<0){
-           temp_hour=23;
+        UTC_OFFSET[0] -= 1;
+        if (UTC_OFFSET[0]<0){
+          UTC_OFFSET[0]=23;
         }
     }
     else if (pressed == "OK"){
         delay(200);
-        hours = temp_hour;
         break;
     }
     else if (pressed == "CANCEL"){
@@ -183,44 +209,73 @@ void set_time(){
         break;
     }
   }
-  int temp_minute = minutes;
   while (true){
     display.clearDisplay();
-    print_line("Enter minute:" + String(temp_minute), 0, 0,2);
+    print_line("Enter minute:" + String(UTC_OFFSET[1]), 0, 0,2);
 
     String pressed = wait_for_button_press();
     if (pressed == "UP"){
       delay(200);
-      temp_minute +=1;
-      temp_minute = temp_minute % 60;
+      UTC_OFFSET[1] +=1;
+      UTC_OFFSET[1] = UTC_OFFSET[1] % 60;
     }
     else if (pressed == "DOWN"){
       delay(200);
-      temp_minute -= 1;
-      if (temp_minute<0){
-           temp_minute=59;
+      UTC_OFFSET[1] -= 1;
+      if (UTC_OFFSET[1]<0){
+        UTC_OFFSET[1]=59;
       }
     }
     else if (pressed == "OK"){
       delay(200);
-      minutes = temp_minute;
+      UTC_OFFSET[1] += (UTC_OFFSET[1]);
       break;
     }
+
     else if (pressed == "CANCEL"){
       delay(200);
       break;
     }
   }
+  int UTC_Offset=UTC_OFFSET[0]*60*60 + UTC_OFFSET[1]*60;
+  display.clearDisplay();
+  print_line("Enter the direction from UTC", 0, 0, 2);
+  print_line("+ up", 0, 16, 2);
+  print_line("- Down", 0, 24, 2);
+  String pressed = wait_for_button_press();
+  if (pressed == "DOWN") {
+    UTC_Offset *= -1;
+  }
+  configTime(UTC_Offset, UTC_OFFSET_DST, NTP_SERVER);
   display.clearDisplay();
   print_line("Time is set", 0, 0, 2);
   delay(1000);
 }
 
+void active_alarm(){
+  display.clearDisplay();
+  int x=0;
+  for (int i=0; i<n_alarms;i++){
+    if (alarm_active[i]==true){
+      
+      print_line("Alarm " + String(i+1) , 1, x, 2);
+      x +=8;
+      print_line( String(i+1) +":"+ String(alarm_hours[i]) +":"+ String(alarm_minutes[i]), 1, x, 0);
+      x +=8;
+    }
+  }
+  delay(1000);
+}
+
 void set_alarm(int alarm){
+  alarm -=1;
+  alarm_active[alarm] = true;
+  alarm_triggered[alarm] = false;
+  alarm_enabled=true;
   int temp_hour = alarm_hours[alarm];
   while (true){
     display.clearDisplay();
-    print_line("Enter hour: " + String(temp_hour), 0, 0, 2);
+    print_line("Enter hour: " + String(temp_hour), 1, 0, 2);
 
     String pressed = wait_for_button_press();
     if (pressed == "UP") {
@@ -248,7 +303,7 @@ void set_alarm(int alarm){
   int temp_minute = alarm_minutes[alarm];
   while (true){
     display.clearDisplay();
-    print_line("Enter minute: " + String(temp_minute),0,0,2);
+    print_line("Enter minute: " + String(temp_minute),1,0,2);
     String pressed = wait_for_button_press();
     if (pressed == "UP"){
       delay(200);
@@ -264,7 +319,7 @@ void set_alarm(int alarm){
     }
     else if (pressed == "OK"){
       delay(200);
-      minutes = temp_minute;
+      alarm_minutes[alarm] = temp_minute;
       break;
     }
     else if (pressed == "CANCEL"){
@@ -273,33 +328,82 @@ void set_alarm(int alarm){
     }
   }
   display.clearDisplay();
-  print_line("Time is set", 0, 0, 2);
-  delay(1000);
+  print_line("Time is set", 1, 0, 2);
+  print_line(String(alarm+1) +":"+ String(alarm_hours[alarm]) +":"+ String(alarm_minutes[alarm]), 1, 8, 0);
+  delay(2000);
+}
+
+void remove_alarm(int alarm){
+  alarm_active[alarm-1]=false;
+  
+  display.clearDisplay();
+  print_line(String(alarm) +" is removed", 2, 4, 0);
+  
 }
 
 void run_mode(int mode){
   if (mode ==0){
+    display.clearDisplay();
+    print_line("Enter alarm you want to remove",1,0,0);
+    print_line("UP-1",1,16,0);
+    print_line("DOWN-2",1,24,0);
+    String pressed2= wait_for_button_press();
+    if (pressed2=="UP"){
+      remove_alarm(1);
+    }
+    else if (pressed2=="DOWN"){
+      remove_alarm(2);
+    }  
+  }
+
+  else if (mode == 1){
     set_time();
   }
-  else if (mode == 1 || mode ==2){
-    set_alarm(mode -1);
+  else if (mode ==2){
+    display.clearDisplay();
+    print_line("Enter alarm (1-2)",1,0,0);
+    print_line("UP-1",1,8,0);
+    print_line("DOWN-2",1,16,0);
+    String pressed1= wait_for_button_press();
+    if (pressed1=="UP"){
+      set_alarm(1);
+      alarm_enabled = true;
+    }
+    else if (pressed1=="DOWN"){
+      set_alarm(2);
+      alarm_enabled = true;
+    }
+    
   }
   else if (mode == 3){
-    alarm_enabled = false;
+    if (alarm_active[0] == true){
+       active_alarm();
+    }
+    else if (alarm_active[1] == true){
+      active_alarm();
+    }
+    else{
+      display.clearDisplay();
+      print_line("No alarms are active",1,0,0);
+    }
   }
 }
 
-void go_to_menu(void){
+void go_to_menu(){
   while (digitalRead(CANCEL)==0){
     display.clearDisplay();
     for (int i=0; i<5;i++){
       print_line("1-set time",1,0,0);
-      print_line("2-set alarm 1",1,8,0);
-      print_line("3-set alarm 2",1,16,0);
+      print_line("2-set alarm",1,8,0);
+      print_line("3-View Active Alarm",1,16,0);
       print_line("4-remove alarm",1,24,0);
       delay(200);
     }
-
+    if (alarm_enabled == true){
+      display.clearDisplay();
+      print_line("00000000000000000",1,0,0);
+      delay(500);
+    }
     String pressed= wait_for_button_press();
     print_line(String(pressed) + "000",1,0,0);
     delay(200);
@@ -310,17 +414,22 @@ void go_to_menu(void){
       if (pressed=="UP"){
         current_mode +=1;
         current_mode %= max_modes;
-        display.clearDisplay();
-        print_line(String(pressed),2,0,0);
-        delay(200);
       }
       else if (pressed == "DOWN"){
         current_mode -= 1;
         if (current_mode < 0){
           current_mode = max_modes -1;
         }
+        
+      }
+      if ( current_mode == 0){
         display.clearDisplay();
-        print_line(String(current_mode),2,0,0);
+        print_line(String(4) ,2,0,0);
+        delay(200);
+      }
+      else{
+        display.clearDisplay();
+        print_line(String(current_mode) ,2,0,0);
         delay(200);
       }
       display.clearDisplay();
@@ -339,22 +448,22 @@ void go_to_menu(void){
 void check_temp(void){
   TempAndHumidity data = dhtSensor.getTempAndHumidity();
   bool all_good = true;
-  if (data.temperature > 35){
+  if (data.temperature > 32){
     all_good = false;
     digitalWrite(LED_2, HIGH);
     print_line("TEMP HIGH", 1, 40, 0);
   }
-  else if(data.temperature < 25){
+  else if(data.temperature < 24){
     all_good = false;
     digitalWrite(LED_2,HIGH);
     print_line("TEMP LOW", 1, 40, 0);
   }
-  if(data.humidity > 85){
+  if(data.humidity > 80){
     all_good = false;
     digitalWrite(LED_2,HIGH);
     print_line("HUMD HIGH", 1, 50, 0);
   }
-  else if(data.humidity < 35){
+  else if(data.humidity < 65){
     all_good = false;
     digitalWrite(LED_2,HIGH);
     print_line("HUMP LOW", 1, 50, 0);
@@ -397,8 +506,8 @@ void setup() {
   }
   display.clearDisplay();
   print_line("Connected to wifi",2 ,0, 0);
-
-  configTime(UTC_OFFSET, UTC_OFFSET_DST, NTP_SERVER);
+  int UTC_Offset=UTC_OFFSET[0];
+  configTime(UTC_Offset, UTC_OFFSET_DST, NTP_SERVER);
 }
 
 void loop() {
